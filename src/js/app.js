fc27516a39d52411e92d1ec19e9d3858ee9ecc22
gwezerek <gwezerek@gmail.com>
2016-06-07 01:20:24 -0400

    Refactor.

--- src/js/app.js
+++ src/js/app.js
@@ -9,56 +9,29 @@ var pageconfig = require('../data/pageconfig.json');
 var $ = require('jquery');
 var swiper = require('swiper');
 
-// $('.slick-ol').slick({
-
-//   // the magic
-//   responsive: [{
-
-//       breakpoint: 1024,
-//       settings: {
-//         slidesToShow: 3,
-//         infinite: true
-//       }
-
-//     }, {
-
-//       breakpoint: 600,
-//       settings: {
-//         slidesToShow: 2,
-//         dots: true
-//       }
-
-//     }, {
-
-//       breakpoint: 300,
-//       settings: 'unslick' // destroys slick
-
-//     }]
-// });
-
-
-
-
-// $('.slick-ol').slick({
-//   arrows: false,
-//   centerMode: true,
-//   variableWidth: true,
-//   slidesToShow: 3,
-//   centerPadding: '100px'
-// });
-
-
 $(document).ready(function () {
-    //initialize swiper when document ready
-    var swiper = new Swiper('.swiper-container', {
-      slidesPerView: 3,
-      // spaceBetween: 10,
-      freeMode: true,
-      freeModeSticky: true,
-      freeModeMomentumRatio: 0.5,
-      centeredSlides: true,
-      loop: true,
-      loopedSlides: 50
-      // spaceBetween: 80
-    });
+
+  var cards = new Swiper('.state-cards', {
+    slidesPerView: 1,
+    freeMode: true,
+    freeModeSticky: true,
+    freeModeMomentumRatio: 0.1,
+    centeredSlides: true,
+    loop: true,
+    loopedSlides: 50
   });
+
+  var picker = new Swiper('.state-picker', {
+    slidesPerView: 4,
+    freeMode: true,
+    freeModeSticky: true,
+    freeModeMomentumRatio: 0.2,
+    centeredSlides: true,
+    loop: true,
+    loopedSlides: 50
+  });
+
+  cards.params.control = picker;
+  picker.params.control = cards;
+
+});
